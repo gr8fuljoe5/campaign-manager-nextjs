0410cf5d2ea40cb5de86a7da0338420d1564a67b
Joe Kanakaraj <jkanakaraj@gmail.com>
2020-10-29 10:57:13 -0400

    fetch WIP

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,9 +1,13 @@
 export default function handler(req, res) {
   if (req.method === "POST") {
+    console.log(res.json);
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify({ status: "SUCCESS" }));
   } else {
     // Handle any other HTTP method
+    res.statusCode = 400;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ status: "ERROR" }));
   }
 }
