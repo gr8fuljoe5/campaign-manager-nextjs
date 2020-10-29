1389dad372032d2db6c413e3c3ab9e8aeb616d7d
Joe Kanakaraj <jkanakaraj@gmail.com>
2020-10-29 14:28:31 -0400

    renaming and theming

--- components/GlobalHeader/index.js
+++ components/GlobalHeader/index.js
@@ -1,22 +1,30 @@
 import AppBar from "@material-ui/core/AppBar";
+import { createStyles, makeStyles } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import React from "react";
 
-// const useStyles = makeStyles((theme) =>
-//   createStyles({
-//     title: {
-//       flexGrow: 1,
-//       color: theme.palette.secondary,
-//     },
-//   })
-// );
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    AppBar: {
+      flexGrow: 1,
+      color: theme.palette.primary.contrastText,
+      backgroundColor: theme.palette.primary.main,
+    },
+    title: {
+      fontSize: theme.typography.h3.fontSize,
+    },
+  })
+);
 
 const Header = () => {
+  const classes = useStyles();
   return (
-    <AppBar position="static">
+    <AppBar position="static" className={classes.AppBar}>
       <Toolbar>
-        <Typography variant="h6">Media Math</Typography>
+        <Typography variant="h1" className={classes.title}>
+          ABC Company
+        </Typography>
       </Toolbar>
     </AppBar>
   );
--- utils/theme.js
+++ utils/theme.js
@@ -4,12 +4,6 @@ import { createMuiTheme } from "@material-ui/core/styles";
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
-    primary: {
-      main: "#556cd6",
-    },
-    secondary: {
-      main: "#19857b",
-    },
     error: {
       main: red.A400,
     },
