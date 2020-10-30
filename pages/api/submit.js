58898460c7a15df747f80afc341631f9f1d60ed9
Joe Kanakaraj <jkanakaraj@gmail.com>
2020-10-29 20:02:48 -0400

    update API to return data

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -1,13 +1,13 @@
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    if (req.body.length > 0) {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify({ status: "SUCCESS" }));
-    } else {
-      res.statusCode = 400;
-      res.setHeader("Content-Type", "application/json");
-      res.end(JSON.stringify({ status: "ERROR" }));
-    }
-  }
+	if (req.method === 'POST') {
+		if (req.body.length > 0) {
+			res.statusCode = 200;
+			res.setHeader('Content-Type', 'application/json');
+			res.end(JSON.stringify({ status: 'SUCCESS', data: req.body }));
+		} else {
+			res.statusCode = 400;
+			res.setHeader('Content-Type', 'application/json');
+			res.end(JSON.stringify({ status: 'ERROR' }));
+		}
+	}
 }
